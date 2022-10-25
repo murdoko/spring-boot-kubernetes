3d10ca49e3e412e0721d236c1d9f23237afcc424
murdoko <murdoko@gmail.com>
2022-10-25 11:37:16 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Service;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.h2.security.SHA256;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Service
 public class UserService {
@@ -13,13 +16,18 @@ public class UserService {
     List<User> users = Arrays.asList(new User(1, "Dara"), new User(2, "Seyha"));
     
     String password = "123456789";
-    
-    byte[] key = "Any String you want".getBytes();
-    SHA256.getHMAC(key, "message");
 
     public List<User> getUser(String containName) {
 
         return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
     }
 
+}
+
+
+@RequestMapping(value = "/")
+public void index(HttpServletResponse res, String value) {
+    res.setHeader("Set-Cookie", value);  // Noncompliant
+    Cookie cookie = new Cookie("jsessionid", value);  // Noncompliant
+    res.addCookie(cookie);
 }
