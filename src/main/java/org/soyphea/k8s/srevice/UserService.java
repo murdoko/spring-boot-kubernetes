ae7ae4f34e2b900bf8e72101f1f55f636812a3ed
murdoko <murdoko@gmail.com>
2022-10-25 10:57:05 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 public class UserService {
 
     List<User> users = Arrays.asList(new User(1, "Dara"), new User(2, "Seyha"));
-
+    String password = "123456789";
     public List<User> getUser(String containName) {
 
         return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
