e185ee864cb9b9f79a68fdb822ea7568b15c3c1a
murdoko <murdoko@gmail.com>
2022-10-27 11:20:31 -0300

    Update vulnerabilidad.java

--- src/main/java/org/soyphea/k8s/vulnerabilidad.java
+++ src/main/java/org/soyphea/k8s/vulnerabilidad.java
@@ -2,13 +2,15 @@ import javax.crypto.Cipher;
 
 public class vulnerabilidad {
     
+    /*
     public static void inyecta_vulnerabilidad() {
         try
         {
-            // Cipher c1 = Cipher.getInstance("DES"); // Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
+            Cipher c1 = Cipher.getInstance("DES"); // Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
         }
         catch(Exception e)
         {
         }
-    }       
+    } 
+    */
 }
