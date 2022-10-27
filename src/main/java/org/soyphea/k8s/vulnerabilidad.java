1f318af687e8a69bacdeab788807a017be22a1db
murdoko <murdoko@gmail.com>
2022-10-27 10:44:20 -0300

    Create vulnerabilidad.java

---
+++ src/main/java/org/soyphea/k8s/vulnerabilidad.java
@@ -0,0 +1,16 @@
+import javax.crypto.Cipher;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.NoSuchPaddingException;
+
+public class vulnerabilidad {
+    
+    public static void inyecta_vulnerabilidad(String[] args) {
+        try
+        {
+            Cipher c1 = Cipher.getInstance("DES"); // Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
+        }
+        catch(NoSuchAlgorithmException|NoSuchPaddingException e)
+        {
+        }
+    }       
+}
